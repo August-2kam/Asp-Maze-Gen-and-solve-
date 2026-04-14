2bf935afb2afbd981337dc0bba04a4f2b9cfb322
Antony Baker <125568081+antBaker@users.noreply.github.com>
2026-04-14 09:09:33 +0200

    Added ASP Solver

---
+++ asp-solver/solve_maze.sh
@@ -0,0 +1,4 @@
+
+clingo maze.lp --outf=2 --rand-freq=1 | jq -r '.Call[0].Witnesses[0].Value[] + "."' > atoms.lp
+clingo maze_solver.lp atoms.lp --outf=2 --rand-freq=1 | jq -r '.Call[0].Witnesses[0].Value[] + "."' > atoms_solved.lp 
+cat atoms.lp atoms_solved.lp > maze_and_solution.txt
